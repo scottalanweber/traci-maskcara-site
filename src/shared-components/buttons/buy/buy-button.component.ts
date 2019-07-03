58d1c0fba7c05195cf01812446318283ae4643ea
Scott Weber <scott.weber@collectivemedicaltech.com>
2019-07-03 04:38:00 -0600

    Update Party Info handling

--- src/shared-components/buttons/buy/buy-button.component.ts
+++ src/shared-components/buttons/buy/buy-button.component.ts
@@ -11,30 +11,49 @@ export class BuyButtonComponent {
   public partyId: string;
   public currentPartyId: string;
   public hostess: string;
-  public partyIds: any = {
+  public partyStatus: string;
+  public partyData: any = {
     'default': '40026',
-    '41651': 'Kaitlyn Lofthouse',
-    '21479': 'Cindy Hansen',
-    '16132': 'Karina Arshad',
-    '22014': 'Megan Watkins',
-    '14841': 'Tess Black',
-    '14175': 'Clarissa Black',
-    '6422': 'Shannon Moss',
-    '21265': '12 Days of Maskcara',
-    '35821': 'A Beautifuleigh YOU Party',
-    '40026': 'June’s Mystery Hostess Party'
+    '41651': {id: '41651', hostess: 'Kaitlyn Lofthouse', status: 'open'},
+    '21479': {id: '21479', hostess: 'Cindy Hansen', status: 'open'},
+    '16132': {id: '16132', hostess: 'Karina Arshad', status: 'open'},
+    '22014': {id: '22014', hostess: 'Megan Watkins', status: 'open'},
+    '14841': {id: '14841', hostess: 'Tess Black', status: 'open'},
+    '14175': {id: '14175', hostess: 'Clarissa Black', status: 'open'},
+    '6422': {id: '6422', hostess: 'Shannon Moss', status: 'open'},
+    '21265': {id: '21265', hostess: '12 Days of Maskcara', status: 'open'},
+    '35821': {id: '35821', hostess: 'A Beautifuleigh YOU Party', status: 'open'},
+    '40026': {id: '40026', hostess: 'June’s Mystery Hostess', status: 'open'},
+    '22012': {id: '22012', hostess: 'Traci Carnette', status: 'closed'},
+    '18906': {id: '18906', hostess: 'Stephanie Dunham', status: 'closed'},
+    '17876': {id: '17876', hostess: 'Traci Weber', status: 'closed'},
+    '14254': {id: '14254', hostess: 'Michelle Williams', status: 'closed'},
+    '13222': {id: '13222', hostess: 'Corinne Thomas', status: 'closed'},
+    '13013': {id: '13013', hostess: 'Traci Weber', status: 'closed'},
+    '10505': {id: '10505', hostess: 'Connie Weber', status: 'closed'},
+    '5190': {id: '5190', hostess: 'Corinne Thomas', status: 'closed'},
+    '2262': {id: '2262', hostess: 'Traci Weber', status: 'closed'},
+    '2261': {id: '2261', hostess: 'Traci Weber', status: 'closed'}
   };
 
   constructor(private modalService: NgbModal) {}
 
   updatePartyId(currentPartyId: any) {
-    this.currentPartyId = currentPartyId;
+    if (currentPartyId !== this.partyData['default']) {
+      this.currentPartyId = currentPartyId;
+    }
+
     this.partyId = currentPartyId;
-    if (currentPartyId in this.partyIds) {
-      this.hostess = this.partyIds[currentPartyId];
+    if (currentPartyId in this.partyData) {
+      this.hostess = this.partyData[currentPartyId].hostess;
+      this.partyStatus = this.partyData[currentPartyId].status;
+      if (this.partyStatus === 'closed') {
+        this.partyId = this.partyData['default'];
+      }
     } else {
-      this.partyId = this.partyIds['default'];
+      this.partyId = this.partyData['default'];
       this.hostess = null;
+      this.partyStatus = null;
     }
   }
 
@@ -43,8 +62,8 @@ export class BuyButtonComponent {
    * @param modal The Modal to be shown
    */
   open(modal: TemplateRef<any>) {
-    this.partyId = this.partyIds['default'];
-    this.updatePartyId(this.partyIds['default']);
+    this.partyId = this.partyData['default'];
+    this.updatePartyId(this.partyData['default']);
     this.modalService.open(modal);
   }
 
