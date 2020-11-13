8b501cfe5683632b41a74d1ea88ab6e43ec75a62
Scott Weber <scott.weber@collectivemedicaltech.com>
2020-11-13 15:48:20 -0700

    Add Tonya's Party and Seint Rebrand

--- src/data/parties.data.ts
+++ src/data/parties.data.ts
@@ -2,6 +2,7 @@ import {PartyData} from '../model/party-data';
 
 export const DEFAULT_PARTY_ID = '113903';
 export const PARTIES: PartyData[] = [
+  {id: '146779', hostess: 'Tonya Wheeler', status: 'open', rewards: false},
   {id: '140627', hostess: 'Mary Begley', status: 'open', rewards: false},
   {id: '139646', hostess: 'Jennifer Behrens', status: 'open', rewards: true},
   {id: '138945', hostess: 'Jamie Swanson', status: 'open', rewards: false},
